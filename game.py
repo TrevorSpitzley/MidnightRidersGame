b40c1d3415b6ce2527ce214acb656aad5c3aff58
Trevor Spitzley <spitzltr@mail.gvsu.edu>
2020-02-03 10:48:37 -0500

    Tilesheet mapped out. Layering fixed.

--- game.py
+++ game.py
@@ -3,26 +3,39 @@
 
 import pygame
 import sys
-
-sys.path.append('.')
 import helperFuncs
+sys.path.append('.')
 from engine.league import league
 from player import Player
 
-
-#This is the event counter for our manually created events, call with evCnt()
+# This is the event counter for our manually created events, call with evCnt()
 evCnt = lambda: helperFuncs.eventNum.newEvent(helperFuncs.eventNum)
+
 
 def quit(self):
     pygame.exit()
     sys.exit()
 
 
+engine = league.Engine("Midnight Riders")
 league.Settings.height = 768
 league.Settings.width = 768
+engine.init_pygame()
+
 # Our tilesheet is 16x16 pixels
 league.Settings.tile_size = 16
-
-tilesheet = league.Spritesheet('', league.Settings.tile_size, )
+# Assign tilesheet
+tilesheet = league.Spritesheet('./TilesetGraveyard_16.png', league.Settings.tile_size, 12)
+# Set tilemaps for blank background and main scene
+scene = league.Tilemap('./ridersScene.lvl', tilesheet, 16, 1)
+scene_size = (scene.wide * league.Settings.tile_size, scene.high * league.Settings.tile_size)
+backdrop = league.Tilemap('./ourBackground.lvl', tilesheet, 16, 0)
+# Add to drawables
+engine.drawables.add(scene.passable.sprites())
+engine.drawables.add(backdrop.passable.sprites())
+# Quit function
+engine.events[pygame.QUIT] = quit
+# Run the engine
+engine.run()
 
 print("THIS IS CONFIRMATION THAT I MADE IT TO THE END OF THE FILE WITHOUT GETTING HUNG UP")
