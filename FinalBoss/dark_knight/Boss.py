a43cc8ed58612d9809baa9d446019f94001f8e25
Trevor Spitzley <spitzltr@mail.gvsu.edu>
2020-02-23 22:58:51 -0500

    Last commit :')

--- FinalBoss/dark_knight/Boss.py
+++ FinalBoss/dark_knight/Boss.py
@@ -13,7 +13,7 @@ class Boss(Character):
         super().__init__(z, x, y)
         self.health = 200  # My Health
         self.lastHit = pygame.time.get_ticks()  # Last booboo
-        self.delta = 128  # BIGGER = FASTER
+        self.delta = (128 * 8)  # BIGGER = FASTER
         self.x = x
         self.y = y
         self._layer = 51
@@ -23,7 +23,7 @@ class Boss(Character):
 
         # Image!!!
         self.image = pygame.image.load('./FinalBoss/dark_knight_frames/idle_frames/frame_00_delay-0.14s.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (36, 36))
+        self.image = pygame.transform.scale(self.image, (60, 60))
         self.rect = self.image.get_rect()
 
         # World size, and collisions
